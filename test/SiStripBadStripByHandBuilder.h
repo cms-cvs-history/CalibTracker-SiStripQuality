8173ea53e63547e68a59a7fe460a200f2a8debb7
alinn <sha1-1ccfb7a1878c5f91cc562c39cc64f98a5d05b691@cern.ch>
2007-07-16 15:18:50 +0000

    Added BadStripByHandBuilder

---
+++ test/SiStripBadStripByHandBuilder.h
@@ -0,0 +1,33 @@
+#ifndef SiStripBadStrip_H
+#define SiStripBadStrip_H
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+
+
+namespace cms{
+  class SiStripBadStripByHandBuilder : public edm::EDAnalyzer {
+
+  public:
+
+    explicit SiStripBadStripByHandBuilder( const edm::ParameterSet& iConfig);
+
+    ~SiStripBadStripByHandBuilder(){};
+
+    virtual void beginJob( const edm::EventSetup& );
+
+    virtual void analyze(const edm::Event& , const edm::EventSetup& );
+
+  private:
+    bool printdebug_;
+    std::vector<uint32_t> ext_bad_detids;
+    std::vector< std::pair<uint32_t, unsigned short> > detid_strips;
+  };
+}
+#endif
